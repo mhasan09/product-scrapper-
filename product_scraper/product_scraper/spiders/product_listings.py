e1da64a3290f8bedc8cbe3cc45ea2d6899286caf
Mahabubul Hasan <shourav6688@gmail.com>
2023-06-08 19:36:22 +0600

    added product listings scraper

--- product_scraper/product_scraper/spiders/product_listings.py
+++ product_scraper/product_scraper/spiders/product_listings.py
@@ -1,19 +1,30 @@
 import scrapy
 
 
-class SubcategorySpider(scrapy.Spider):
+class ProductsSpider(scrapy.Spider):
     name = 'product_listings'
     start_urls = ['https://www.luluhypermarket.com/en-ae/electronics']
 
     def parse(self, response):
-        subcategories = response.css('div.col-lg-2.col-md-2.col-auto')  # getting all the subcategories section
+        subcategories = response.css('div.col-lg-2.col-md-2.col-auto')  # getting all the subcategories
+        all_subcategories_url = subcategories.css("div.col-lg-2.col-md-2.col-auto a::attr(href)").getall()  # getting all the subcategories url
 
-        for subcategory in subcategories:
+        for product in all_subcategories_url:
+            yield response.follow(product, callback=self.parse_products)  # callback the parse_product method to parse the product data leads from subcategory page
 
+    def parse_products(self, response):
+        products = response.css("div.product-desc")  # getting all the items from products section
+
+        for product in products:
+            title = product.css("h3::text").get()
+            price = products.css('span.old-price::text').extract_first()
             yield {
-                "subcategory": subcategory.css("div.col-lg-2.col-md-2.col-auto .img-caption::text").get(),
-                "url": subcategory.css("div.col-lg-2.col-md-2.col-auto a::attr(href)").get()
+                "title": title.replace('\n', ''),
+                "price": "AED " + price,
             }
+
+
+
 
 
 
--- product_scraper/product_scraper/spiders/subcategory_listings.py
+++ product_scraper/product_scraper/spiders/subcategory_listings.py
@@ -6,13 +6,15 @@ class SubcategorySpider(scrapy.Spider):
     start_urls = ['https://www.luluhypermarket.com/en-ae/electronics']
 
     def parse(self, response):
+        base_url = "https://www.luluhypermarket.com"
+
         subcategories = response.css('div.col-lg-2.col-md-2.col-auto')  # getting all the subcategories section
 
         for subcategory in subcategories:
 
             yield {
                 "subcategory": subcategory.css("div.col-lg-2.col-md-2.col-auto .img-caption::text").get(),
-                "url": subcategory.css("div.col-lg-2.col-md-2.col-auto a::attr(href)").get()
+                "url": base_url + subcategory.css("div.col-lg-2.col-md-2.col-auto a::attr(href)").get()
             }
 
 
